8808b4f4ca6e088100ced915c39015f36cfda857
yellmii <choiyl930@naver.com>
2022-07-28 15:22:31 +0900

    2022/07/28

--- main.js
+++ main.js
@@ -1,83 +1,114 @@
 let news = [];
-let today = new Date();
-let todayFormat = document.getElementById("today-format");
+let menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) => menu.addEventListener("click", (event) => getNewsByTopic(event)));
 
-let year = today.getFullYear();
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-let month = months[today.getMonth()];
-let date = ('0' + today.getDate()).slice(-2);
-const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-let day = week[today.getDay()];
+let searchButton = document.getElementById("search-button");
 
-let dateString = day + ', ' + month + ' ' + date  + ', ' + year;
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+};
 
-todayFormat.textContent = `${dateString}`;
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+const openInputBox = () => {
+    let inputArea = document.getElementById("input-area");
+
+    if(inputArea.style.display === "inline") {
+        inputArea.style.display = "none";
+    } else {
+        inputArea.style.display = "inline";
+    }
+};
 
 const getLatestNews = async () => {
-    let url= new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=15`);
-    let url_sub= new URL(`https://api.newscatcherapi.com/v2/search?q=Apple&from='2022/7/15'&countries=CA&page_size=15`);
+    let url = new URL(
+        `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10`
+    );
+    let header = new Headers({
+        "x-api-key": "5aig2uQWkrzp3yUZvMDmeu7jFpEubqONvxofVMKFABs"
+    });
 
-    let header = new Headers({"x-api-key":"5aig2uQWkrzp3yUZvMDmeu7jFpEubqONvxofVMKFABs"});
-    
-    let response = await fetch(url, {headers:header});
+    let response = await fetch(url, { headers: header });
     let data = await response.json();
-
-    let response_sub = await fetch(url_sub, {headers:header});
-    let data_sub = await response_sub.json();
-    
     news = data.articles;
-    news_sub = data_sub.articles;
+    console.log(news);
+
+    render();
+};
+
+const getNewsByTopic = async () => {
+    let topic = event.target.textContent.toLowerCase();
+    let url = new URL(
+        `https://api.newscatcherapi.com/v2/latest_headlines?countries=US&&page_size=10&topic=${topic}`
+    );
+    let header = new Headers({
+        "x-api-key": "5aig2uQWkrzp3yUZvMDmeu7jFpEubqONvxofVMKFABs"
+    });
+
+    let response = await fetch(url, { headers: header });
+    let data = await response.json();
+    news = data.articles;
+    console.log(news);
+
+    render();
+};
+
+const getNewsBySearch = async () => {
+    let searchInput = document.getElementById("search-input").value;
+    console.log("key", searchInput);
+    let url = new URL(
+        `https://api.newscatcherapi.com/v2/search?q=${searchInput}&countries=US&page_size=10`
+    );
+    console.log("check", searchInput.textContent);
+    let header = new Headers({
+        "x-api-key": "5aig2uQWkrzp3yUZvMDmeu7jFpEubqONvxofVMKFABs"
+    });
+
+    let response = await fetch(url, { headers: header });
+    let data = await response.json();
+    news = data.articles;
+    console.log(news);
 
     render();
 };
 
 const render = () => {
     let newsHTML = '';
-    let newsHTML_sub = '';
 
-    newsHTML = news.map((item) => { 
+    newsHTML = news.map((items) => {
         return `
-        <div class="row left-news">
-            <div class="col-4 layout">
-                <div>
-                    <div class="main-title">${item.title}</div>
-                    <div class="sub-title">${
-                        item.summary == null || item.summary == ""
-                        ? ""
-                        : item.summary.length > 200
-                        ? item.summary.substring(0, 200) + "..."
-                        : item.summary
-                    }</div>
-                </div>
-            </div>
-            <div class="col-8">
-                <img class="news-img-size" src="${item.media}"/>
-                <div class="news-rights">${item.rights}</div>
-            </div>
-        </div>
-    `
-    }).join('');
+        <div class="row news">
+        <div class="col-lg-4">
+                        <img class="news-img-size" src="${
+                            items.media ||
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+                }"/>
+                    </div>
+                    <div class="col-lg-8">
+                        <h2 class="news-title">${items.title}</h2>
+                        <p class="news-summary">${
+                            items.summary == null || items.summary == ""
+                            ? ""
+                            : items.summary.length > 200
+                            ? items.summary.substring(0, 200) + "..."
+                            : items.summary
+                        }</p>
+                        <div class="rights">${
+                            items.rights == null || items.rights == ""
+                            ? "no source"
+                            : items.right
+                        }
+                             * ${items.published_date}</div>
+                    </div>
+                    </div>
+        `;
+    }).join("");
 
-    newsHTML_sub = news_sub.map((item_sub) => { 
-        return `
-        <div class="row right-news">
-            <img class="news-img-size" src="${item_sub.media}"/>
-            <div class="news-rights">${item_sub.rights}</div>
-            <div class="main-title">${item_sub.title}</div>
-            <div class="sub-title">${
-                item_sub.summary == null || item_sub.summary == ""
-                ? ""
-                : item_sub.summary.length > 100
-                ? item_sub.summary.substring(0, 100) + "..."
-                : item_sub.summary
-            }</div>
-        </div>
-    `
-    }).join('');
-
-    document.getElementById("left-news-board").innerHTML = newsHTML;
-    document.getElementById("right-news-board").innerHTML = newsHTML_sub;
+    document.getElementById("news-board").innerHTML = newsHTML;
 };
 
-getLatestNews();
 
+searchButton.addEventListener("click", getNewsBySearch);
+getLatestNews();
